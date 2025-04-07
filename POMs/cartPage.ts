2727c468b90a1d1ebc5b18fe1fa99b5a8bc505cb
Katarina Đurđević <kdurdevic0508@gmail.com>
2025-04-07 11:02:13 +0200

    refactoring existing tests, add order test

--- POMs/cartPage.ts
+++ POMs/cartPage.ts
@@ -10,6 +10,16 @@ export class CartPage {
     laptopItem: Locator;
     monitorItem: Locator;
     deleteLink: Locator;
+    placeOrderButton: Locator;
+    nameField: Locator;
+    countryField: Locator;
+    cityField: Locator;
+    creditCardField: Locator;
+    monthField: Locator;
+    yearField: Locator;
+    purchaseButton: Locator;
+    okButton: Locator;
+    successMessage: Locator;
 
 
     constructor(page: Page) {
@@ -20,9 +30,19 @@ export class CartPage {
         this.laptopItem = page.getByRole('cell', { name: 'Apple monitor' });
         this.monitorItem = page.getByRole('cell', { name: 'Samsung galaxy s6' });
         this.deleteLink = page.getByRole('row', { name: 'Sony vaio i5 790 Delete' }).getByRole('link');
+        this.placeOrderButton = page.getByRole('button', { name: 'Place Order' });
+        this.nameField = page.getByRole('textbox', { name: 'Total: Name:' });
+        this.countryField = page.getByRole('textbox', { name: 'Country:' });
+        this.cityField = page.getByRole('textbox', { name: 'City:' });
+        this.creditCardField = page.getByRole('textbox', { name: 'Credit card:' });
+        this.monthField = page.getByRole('textbox', { name: 'Month:' });
+        this.yearField = page.getByRole('textbox', { name: 'Year:' });
+        this.purchaseButton = page.getByRole('button', { name: 'Purchase' });
+        this.okButton = page.getByRole('button', { name: 'OK' });
+        this.successMessage = page.getByRole('heading', { name: 'Thank you for your purchase!' });
     }
 
-    public async openCart(){
+    public async openCart() {
         await this.cartLink.click();
     }
 
@@ -30,20 +50,44 @@ export class CartPage {
         await expect(this.page).toHaveURL('https://www.demoblaze.com/cart.html');
     }
 
-    public async assertItemsAreInCart(){
+    public async assertItemsAreInCart() {
         await expect(this.phoneItem).toBeVisible();
         await expect(this.laptopItem).toBeVisible();
         await expect(this.monitorItem).toBeVisible();
     }
 
-    public async deleteLaptopItem(){
+    public async deleteLaptopItem() {
         await this.deleteLink.click();
     }
 
-    public async assertLaptopIsDeletedFromCart(){
+    public async assertLaptopIsDeletedFromCart() {
         await expect(this.laptopItem).toHaveCount(0);
     }
 
+    public async openOrderModal() {
+        await this.placeOrderButton.click();
+    }
+
+    public async fulfillOrderInformation(name: string, country: string, city: string, creditCard: string, month: string, year: string) {
+        await this.nameField.fill(name);
+        await this.countryField.fill(country);
+        await this.cityField.fill(city);
+        await this.creditCardField.fill(creditCard);
+        await this.monthField.fill(month);
+        await this.yearField.fill(year);
+    }
+
+    public async submitOrder() {
+        await this.purchaseButton.click();
+    }
+
+    public async assertPurchaseIsSuccessful() {
+        await expect(this.successMessage).toHaveText("Thank you for your purchase!");
+    }
+
+    public async closeSuccessModal() {
+        await this.okButton.click();
+    }
 
 }
 
--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { HomePage } from '../POMs/homePage';
 import { CartPage } from '../POMs/cartPage';
 import path from 'path';
+import { city, country, creditCard, month, name, year } from '../utils/order_information';
 
 dotenv.config();
 
@@ -12,41 +13,60 @@ test.beforeEach(async ({ page }) => {
     await page.goto('/');
 });
 
-test('Adding items to cart', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const cartPage = new CartPage(page);
+test.describe.serial('add, delete, order', () => {
+    test('Add items to cart ', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const cartPage = new CartPage(page);
 
-    //given
-    await homePage.assertUserIsLoggedIn();
+        //given
+        await homePage.assertUserIsLoggedIn();
 
-    //when
-    await homePage.addPhoneItemToCart();
-    await homePage.assertPhoneItemAddedToCart('Product added');
-    await homePage.addMonitorItemToCart();
-    await homePage.assertMonitorItemAddedToCart('Product added');
+        //when
+        await homePage.addPhoneItemToCart();
+        await homePage.assertPhoneItemAddedToCart('Product added');
+        await homePage.addMonitorItemToCart();
+        await homePage.assertMonitorItemAddedToCart('Product added');
+        await homePage.addLaptopItemToCart();
+        await homePage.assertLaptopItemAddedToCart('Product added');
 
-    //then 
-    await cartPage.openCart();
-    await cartPage.assertCartIsOpened();
-    await cartPage.assertItemsAreInCart();
+        //then 
+        await cartPage.openCart();
+        await cartPage.assertCartIsOpened();
+        await cartPage.assertItemsAreInCart();
+    });
 
-});
+    test('Delete item from cart', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const cartPage = new CartPage(page);
 
-test('Add and delete item from cart', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const cartPage = new CartPage(page);
+        //given
+        await homePage.assertUserIsLoggedIn();
 
-    //given
-    await homePage.assertUserIsLoggedIn();  
-    await homePage.addLaptopItemToCart();
-    await homePage.assertLaptopItemAddedToCart('Product added'); 
+        //when
+        await cartPage.openCart();
+        await cartPage.assertCartIsOpened();
 
-    //when
-    await cartPage.openCart();
-    await cartPage.assertCartIsOpened(); 
-    await cartPage.deleteLaptopItem();
+        //then 
+        await cartPage.deleteLaptopItem();
+        await cartPage.assertLaptopIsDeletedFromCart();
+    });
 
-    //then 
-    await cartPage.assertLaptopIsDeletedFromCart();
+    test('Order items', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const cartPage = new CartPage(page);
 
+        //given
+        await homePage.assertUserIsLoggedIn();
+        await cartPage.openCart();
+        await cartPage.assertCartIsOpened();
+
+        //when
+        await cartPage.openOrderModal();
+        await cartPage.fulfillOrderInformation(name, country, city, creditCard, month, year);
+        await cartPage.submitOrder();
+
+        //then 
+        await cartPage.assertPurchaseIsSuccessful();
+        await cartPage.closeSuccessModal();
+    });
 });
